2f0bc84583681be1e4b4ada0e2f5b870ce85660c
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-05-16 14:03:10 -0400

    Main commits

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank"></a>
-        <a href="https://react.dev" target="_blank"></a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">Book Searcher</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
