b337ad8e625932eaab438f48ac40545b42154469
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-08-16 14:21:14 -0400

    Footer 1.1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="w-full p-1 bg-[#758173] bg-opacity-30 shadow md:justify-between md:p-1 mt-20 text-center">
-      <p>Aviad Churaman © 2025</p>
+      <p className="text-[#A9C5A0]">Aviad Churaman © 2025</p>
     </footer>
   );
 }
