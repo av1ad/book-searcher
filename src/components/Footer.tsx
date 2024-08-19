c87c32a2a3f8142f4b83b59ac262638bb4b5b402
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-08-19 08:02:06 -0400

    Home page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="w-full p-1 bg-[#758173] bg-opacity-30 shadow md:justify-between md:p-1 mt-20 text-center">
+    <footer className="w-full p-1 bg-[#758173] bg-opacity-30 shadow md:justify-between md:p-1 text-center">
       <p className="text-[#A9C5A0]">Aviad Churaman © 2025</p>
     </footer>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,19 @@
+import { FaSearch } from "react-icons/fa";
+
 export default function Home() {
     return (
-        <div>
+        <div className="h-screen">
             <div>
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
+                <ul className="flex justify-between">
+                    <li className="border-8 w-[224px] h-[350px] m-7"></li>
+                    <li className="border-8 w-[224px] h-[350px] m-7"></li>
+                    <li className="border-8 w-[224px] h-[350px] m-7"></li>
+                    <li className="border-8 w-[224px] h-[350px] m-7"></li>
                 </ul>
             </div>
             <div>
                 <input type="text" placeholder="Search for books" />
-                <button>Search</button>
+                <button><FaSearch /></button>
             </div>
         </div>
     )
