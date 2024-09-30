c10c2ba866948212c870b1ec94be0fee789f591f
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-09-30 09:12:28 -0400

    Added author page

--- src/components/author/authors.tsx
+++ src/components/author/authors.tsx
@@ -7,7 +7,18 @@ export default function authors() {
     <>
       <Header />
       <div className="h-screen">
-        <h1>Authors</h1>
+        <div>
+          <div className="bg-[#D9D9D9] bg-opacity-10 mt-10 mx-auto h-48 w-48"></div>
+            <h1 className="text-2xl p-3 text-white uppercase bold">Author Name</h1>
+        </div>
+        <div className="books">
+          <ul className="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 justify-items-center">
+            <li className="border-[.5em] w-[12em] h-[19em] m-3"></li>
+            <li className="border-[.5em] w-[12em] h-[19em] m-3"></li>
+            <li className="border-[.5em] w-[12em] h-[19em] m-3"></li>
+            <li className="border-[.5em] w-[12em] h-[19em] m-3"></li>
+          </ul>
+        </div>
       </div>
       <Footer />
     </>
