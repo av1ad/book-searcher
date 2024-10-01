807603d734d6be991d9edecdd9ce0bac0e652a18
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-10-01 13:12:20 -0400

    Edited navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   // State to manage the Header's visibility
@@ -15,33 +14,45 @@ const Header = () => {
 
   // Array containing navigation items
   const navItems = [
-    { id: 1, text: 'Home', link: '/', target: '_self'},
-    { id: 2, text: 'Genres', link: '/genres', target: '_self'},
-    { id: 3, text: 'Authors', link: '/authors', target: '_self'},
-    { id: 4, text: 'Randomizer', link: '/randomizer', target: '_self'},
-    { id: 5, text: 'AI', link: '/ai', target: '_self'},
-    { id: 6, text: <FaGithub size={25} />, link: 'https://github.com/jewelsonmyjeans', target: '_blank'},
+    { id: 1, text: "Home", link: "/", target: "_self" },
+    { id: 2, text: "Genres", link: "/genres", target: "_self" },
+    { id: 3, text: "Authors", link: "/authors", target: "_self" },
+    { id: 4, text: "Randomizer", link: "/randomizer", target: "_self" },
+    { id: 5, text: "AI", link: "/ai", target: "_self" },
+    {
+      id: 6,
+      text: <FaGithub size={25} />,
+      link: "https://github.com/jewelsonmyjeans",
+      target: "_blank",
+    },
   ];
 
   return (
-    <div className='bg-[#758173] flex justify-between items-center h-24 max-w-full mx-auto px-8 text-white'>
+    <div className="bg-[#758173] flex justify-between items-center h-24 max-w-full mx-auto px-8 text-white">
       {/* Logo */}
-      <h1 className='w-[75%] text-3xl font-bold text-[#FFFFFF] text-left'>BookSearcher </h1>
+      <h1 className="w-[75%] text-3xl font-bold text-[#FFFFFF] text-left">
+        BookSearcher{" "}
+      </h1>
 
       {/* Desktop Navigation */}
-      <ul className='hidden md:flex'>
-        {navItems.map(item => (
+      <ul className="hidden md:flex">
+        {navItems.map((item) => (
           <li
             key={item.id}
-            className='p-4 hover:bg-[#A9C5A0] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
+            className="p-4 rounded-xl m-2 cursor-pointer duration-300 hover:text-black"
           >
-            <Link to={item.link}>{item.text}</Link>
+            <NavLink
+              to={item.link}
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              {item.text}
+            </NavLink>
           </li>
         ))}
       </ul>
 
       {/* Mobile Navigation Icon */}
-      <div onClick={handleNav} className='block md:hidden'>
+      <div onClick={handleNav} className="block md:hidden">
         {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
 
@@ -49,20 +60,22 @@ const Header = () => {
       <ul
         className={
           nav
-            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#758173] ease-in-out duration-500'
-            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+            ? "fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#758173] ease-in-out duration-500"
+            : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
         }
       >
         {/* Mobile Logo */}
-        <h1 className='w-[90%] text-3xl font-bold text-[#FFFFFF] m-4'>BookSearcher</h1>
+        <h1 className="w-[90%] text-3xl font-bold text-[#FFFFFF] m-4">
+          BookSearcher
+        </h1>
 
         {/* Mobile Navigation Items */}
-        {navItems.map(item => (
+        {navItems.map((item) => (
           <li
             key={item.id}
-            className='p-4 border-b rounded-xl hover:bg-[#A9C5A0] duration-300 hover:text-black cursor-pointer border-gray-600'
+            className="p-4 border-b rounded-xl hover:bg-[#A9C5A0] duration-300 hover:text-black cursor-pointer border-gray-600"
           >
-            <Link to={item.link}>{item.text}</Link>
+            <NavLink to={item.link} className={({ isActive }) => (isActive ? "active" : "")}>{item.text}</NavLink>
           </li>
         ))}
       </ul>
--- src/components/randomizer/randomizer.tsx
+++ src/components/randomizer/randomizer.tsx
@@ -8,7 +8,7 @@ export default function randomizer() {
       <Header />
       <div className="flex flex-col min-h-screen justify-center items-center">
         <h1>Get A Random Book!</h1>
-        <li className="border-[.5em] w-[12em] h-[19em] mx-auto m-3 list-none"></li>
+        <li className="border-[.5em] w-[12em] h-[19em] m-3 list-none"></li>
         <button className="bg-[#758173] text-[#d9d9d9] p-3 rounded w-48">Randomize</button>
       </div>
       <Footer />
